c3774a08625fea9710360fbdefd7c33a1ebadbff
audrbar <audrbar@gmail.com>
2023-10-29 22:47:14 +0200

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -8,6 +12,9 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
+      <Projects />
+      <Skills />
+      <Experience />
     </main>
   );
 }
