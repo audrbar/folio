65d81530ea682c19870c95c295656c7d00021e6a
audrbar <audrbar@gmail.com>
2023-10-30 20:23:50 +0200

    z-index

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'audrbar | Personal Portfolio',
-  description: 'Full-stack developer with 3 years of experience.',
+  description: 'Full-stack developer with 3 years of learning experience.',
 }
 
 export default function RootLayout({
@@ -22,7 +22,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[7rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
         <ThemeContextProvider>
